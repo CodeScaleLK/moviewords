90a51f9f887e5ec2c64fe34eeb85bf2520d78870
Lakpriya Senevirathna <lakpriya1024@gmail.com>
2021-09-21 21:49:49 +0530

    ui-n

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import "./add-to-home.scss";
 import Word from "./components/word";
 import { initDB, useIndexedDB } from "react-indexed-db";
 import { DBConfig } from "./DBConfig";
+import AddToHomeScreen from "@ideasio/add-to-homescreen-react";
+import logo from "./images/icons/logo512.png";
+
 const unique = require("unique-words");
 
 initDB(DBConfig);
@@ -45,59 +49,109 @@ const App = () => {
   };
 
   return (
-    <div className="content">
-      <div className="title">
-        <h2>Word Library</h2>
-        <h4>Learn new english words, before watching a film!</h4>
-        <div className="file-input">
-          <label className="file-button" htmlFor="file">
-            Upload Subtitle
-          </label>
-          <input
-            type="file"
-            name="file"
-            id="file"
-            onChange={(e) => readFile(e)}
-          />
+    <>
+      <div className="content">
+        <div className="title">
+          <h2>Word Library</h2>
+          <h4>Learn new english words, before watching a film!</h4>
+          <div className="file-input">
+            <label className="file-button" htmlFor="file">
+              Upload Subtitle
+            </label>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              onChange={(e) => readFile(e)}
+            />
+          </div>
         </div>
-      </div>
-      <div className="words-counts">
-        <p>{`New Words: ${words.length}`}</p>
-        <p>{`Learnd Words: ${existingWords?.length}`}</p>
-      </div>
-      {words.length !== 0 && (
-        <>
-          <p className="sub-title red-color">New words in this file!</p>
-          <div className="words-box">
-            {words.map((item, index) => (
-              <Word disable={false} option={1} key={index} item={item} />
-            ))}
-          </div>
-        </>
-      )}
-      {removedWords && removedWords.length !== 0 && (
-        <>
-          <p className="sub-title blue-color">Learnd words in this file!</p>
-          <div className="words-box">
-            {removedWords?.map((item, index) => (
-              <Word disable option={2} key={index} item={item} />
-            ))}
-          </div>
-        </>
-      )}
+        <div className="words-counts">
+          <p>{`New Words: ${words.length}`}</p>
+          <p>{`Learned Words: ${existingWords?.length}`}</p>
+        </div>
+        {words.length !== 0 && (
+          <>
+            <p className="sub-title red-color">New words in this file!</p>
+            <div className="words-box">
+              {words.map((item, index) => (
+                <Word disable={false} option={1} key={index} item={item} />
+              ))}
+            </div>
+          </>
+        )}
+        {removedWords && removedWords.length !== 0 && (
+          <>
+            <p className="sub-title blue-color">Learned words in this file!</p>
+            <div className="words-box">
+              {removedWords?.map((item, index) => (
+                <Word disable option={2} key={index} item={item} />
+              ))}
+            </div>
+          </>
+        )}
 
-      {existingWords && existingWords.length !== 0 && (
-        <>
-          <p className="sub-title green-color">All Words you know!</p>
-          <div className="words-box">
-            {existingWords.map((item, index) => (
-              <Word disable option={3} key={index} item={item} />
-            ))}
-          </div>
-          <h5 className="copy-name">© Lakpriya Senevirathna</h5>
-        </>
-      )}
-    </div>
+        {existingWords && existingWords.length !== 0 && (
+          <>
+            <p className="sub-title green-color">All words you already know!</p>
+            <div className="words-box">
+              {existingWords.map((item, index) => (
+                <Word disable option={3} key={index} item={item} />
+              ))}
+            </div>
+          </>
+        )}
+        {((existingWords && existingWords.length !== 0) ||
+          words.length !== 0) && (
+          <>
+            <h5 className="made-with">Made with ❤️</h5>
+            <h6 className="copy-name">
+              © Lakpriya Senevirathna
+              <br />
+              CodeScale
+            </h6>
+          </>
+        )}
+      </div>
+      <AddToHomeScreen
+        appId="Word Library"
+        startAutomatically={true}
+        startDelay={0}
+        lifespan={300}
+        debug={true}
+        skipFirstVisit={false}
+        displayPace={0}
+        customPromptContent={{
+          title: "Do you want to install Word Library on your device?",
+          cancelMsg: "",
+          installMsg: "Yes, sure!",
+          guidanceCancelMsg: "Dismiss",
+          src: logo,
+        }}
+        customPromptElements={{
+          container: "athContainer",
+          containerAddOns: "",
+          banner: "athBanner",
+          logoCell: "athLogoCell",
+          logoCellAddOns: "athContentCell",
+          logo: "athLogo",
+          titleCell: "athTitleCell",
+          titleCellAddOns: "athContentCell",
+          title: "athTitle",
+          cancelButtonCell: "athCancelButtonCell",
+          cancelButtonCellAddOns: "athButtonCell",
+          cancelButton: "athCancelButton",
+          installButtonCell: "athInstallButtonCell",
+          installButtonCellAddOns: "athButtonCell",
+          installButton: "athInstallButton",
+          installButtonAddOns: "button",
+          guidance: "athGuidance",
+          guidanceImageCell: "athGuidanceImageCell",
+          guidanceImageCellAddOns: "",
+          guidanceCancelButton: "athGuidanceCancelButton",
+        }}
+      />
+    </>
   );
 };
 
